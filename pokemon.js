55113b07af4114b6252c377f2bd28e9250e7a8d3
maxxapok <maxxapok@mail.ru>
2020-10-05 23:48:14 +0300

    добавила условия

--- pokemon.js
+++ pokemon.js
@@ -1,3 +1,7 @@
+// Задача 1
+// Тебе нужно создать запрос на получение всех покемонов и вывести на экран их кол-во
+// Если возникнет ошибка - вывести текст ошибки, что при получении всех покемонов возникла ошибка
+
 fetch('http://pokemon---api.herokuapp.com/api/v1/pokemon')
   .then(response => response.json())
   .then(data => console.log(data.length))
